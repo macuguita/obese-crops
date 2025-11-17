b2632db585e32a219c00d75c806a0625178497f2
macuguita <raulpripri@gmail.com>
2025-11-17 22:19:29 +0100

    change flake.nix

--- flake.nix
+++ flake.nix
@@ -1,41 +1,33 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      devShells = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-          libs = with pkgs; [
-            libpulseaudio
-            libGL
-            glfw
-            openal
-            flite
-            stdenv.cc.cc.lib
+        libs = with pkgs; [
+          libpulseaudio
+          libGL
+          glfw
+          openal
+          flite
+          stdenv.cc.cc.lib
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            jdk
           ];
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              jdk
-              git
 
-              # Custom scripts
-              nameReplacerScript
-            ];
+          buildInputs = libs;
 
-            buildInputs = libs;
-
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
-          };
-        });
-    };
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
+        };
+      }
+    );
 }
